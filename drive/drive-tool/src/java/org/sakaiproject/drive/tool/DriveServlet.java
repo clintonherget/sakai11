2e4a3bd937ae0f3d1787cbf2073f45e45474c926
Mark Triggs <mark@dishevelled.net>
2017-09-26 08:45:10 +1000

    Drive skeleton import

---
+++ drive/drive-tool/src/java/org/sakaiproject/drive/tool/DriveServlet.java
@@ -0,0 +1,250 @@
+/**********************************************************************************
+ *
+ * Copyright (c) 2015 The Sakai Foundation
+ *
+ * Original developers:
+ *
+ *   New York University
+ *   Payten Giles
+ *   Mark Triggs
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
+package org.sakaiproject.drive.tool;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.Options;
+import com.github.jknack.handlebars.Template;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.sakaiproject.authz.cover.SecurityService;
+import org.sakaiproject.component.cover.ComponentManager;
+import org.sakaiproject.component.cover.ServerConfigurationService;
+import org.sakaiproject.time.api.Time;
+import org.sakaiproject.time.cover.TimeService;
+import org.sakaiproject.tool.api.Session;
+import org.sakaiproject.tool.cover.SessionManager;
+import org.sakaiproject.tool.cover.ToolManager;
+import org.sakaiproject.user.cover.PreferencesService;
+import org.sakaiproject.user.cover.UserDirectoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.sakaiproject.drive.tool.handlers.*;
+
+public class DriveServlet extends HttpServlet {
+
+    private static final String GOOGLE_DOMAIN = "gqa.nyu.edu";
+
+    private static final Logger LOG = LoggerFactory.getLogger(DriveServlet.class);
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // What's the plan here?
+        // Need to get a token for the current user (auth flow if they've not yet done that) {
+        // Then fetch a list of their resources {
+        // And stick them on the page...
+
+        String googleUser = getCurrentGoogleUser();
+
+        I18n i18n = new I18n(this.getClass().getClassLoader(), "org.sakaiproject.drive.tool.i18n.drive");
+
+        response.setHeader("Content-Type", "text/html");
+
+        URL toolBaseURL = determineBaseURL();
+        Handlebars handlebars = loadHandlebars(toolBaseURL, i18n);
+
+        // toolBaseURL
+        System.err.println("\n*** DEBUG " + System.currentTimeMillis() + "[DriveServlet.java:90 3221a]: " + "\n    toolBaseURL => " + (toolBaseURL) + "\n");
+
+        try {
+            Template template = handlebars.compile("org/sakaiproject/drive/tool/views/layout");
+            Map<String, Object> context = new HashMap<String, Object>();
+
+            context.put("baseURL", toolBaseURL);
+            context.put("layout", true);
+            context.put("skinRepo", ServerConfigurationService.getString("skin.repo", ""));
+            context.put("randomSakaiHeadStuff", request.getAttribute("sakai.html.head"));
+            context.put("googleUser", getCurrentGoogleUser());
+
+            Handler handler = handlerForRequest(request);
+
+            handler.handle(request, response, context);
+
+            if (handler.hasRedirect()) {
+                if (handler.getRedirect().startsWith("http")) {
+                    response.sendRedirect(handler.getRedirect());
+                } else {
+                    response.sendRedirect(toolBaseURL + handler.getRedirect());
+                }
+            } else {
+                if (Boolean.TRUE.equals(context.get("layout"))) {
+                    response.getWriter().write(template.apply(context));
+                }
+            }
+        } catch (IOException e) {
+            LOG.warn("Write failed", e);
+        }
+    }
+
+    private Handler handlerForRequest(HttpServletRequest request) {
+        String path = request.getPathInfo();
+
+        if (path == null) {
+            path = "";
+        }
+
+        GoogleClient google = new GoogleClient();
+
+        if (path.contains("/handle-google-login")) {
+            return new OAuthHandler(OAuthHandler.HANDLE_OAUTH);
+        } else if (google.getCredential(getCurrentGoogleUser()) == null) {
+            return new OAuthHandler(OAuthHandler.SEND_TO_GOOGLE);
+        }
+
+        return new IndexHandler();
+    }
+
+    private URL determineBaseURL() {
+        String siteId = null;
+        String toolId = null;
+
+        if (ToolManager.getCurrentPlacement() != null) {
+            siteId = ToolManager.getCurrentPlacement().getContext();
+            toolId = ToolManager.getCurrentPlacement().getId();
+        }
+
+        try {
+            String result = ServerConfigurationService.getPortalUrl();
+
+            if (siteId != null) {
+                result += "/site/" + siteId;
+                if (toolId != null) {
+                    result += "/tool/" + toolId + "/";
+                }
+            }
+
+            return new URL(result);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Couldn't determine tool URL", e);
+        }
+    }
+
+    private String getCurrentGoogleUser() {
+        return UserDirectoryService.getCurrentUser().getEid() + "@" + GOOGLE_DOMAIN;
+    }
+
+    private Handlebars loadHandlebars(final URL baseURL, final I18n i18n) {
+        Handlebars handlebars = new Handlebars();
+
+        handlebars.registerHelper("subpage", new Helper<Object>() {
+            @Override
+            public CharSequence apply(final Object context, final Options options) {
+                String subpage = options.param(0);
+                try {
+                    Template template = handlebars.compile("org/sakaiproject/drive/tool/views/" + subpage);
+                    return template.apply(context);
+                } catch (IOException e) {
+                    LOG.warn("IOException while loading subpage", e);
+                    return "";
+                }
+            }
+        });
+
+        handlebars.registerHelper("show-time", new Helper<Object>() {
+            @Override
+            public CharSequence apply(final Object context, final Options options) {
+                long utcTime = options.param(0) == null ? 0 : options.param(0);
+
+                if (utcTime == 0) {
+                    return "-";
+                }
+
+                Time time = TimeService.newTime(utcTime);
+
+                return time.toStringLocalFull();
+            }
+        });
+
+        handlebars.registerHelper("actionURL", new Helper<Object>() {
+            @Override
+            public CharSequence apply(final Object context, final Options options) {
+                String type = options.param(0);
+                String uuid = options.param(1);
+                String action = options.param(2);
+
+                try {
+                    return new URL(baseURL, type + "/" + uuid + "/" + action).toString();
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException("Failed while building action URL", e);
+                }
+            }
+        });
+
+        handlebars.registerHelper("newURL", new Helper<Object>() {
+            @Override
+            public CharSequence apply(final Object context, final Options options) {
+                String type = options.param(0);
+                String action = options.param(1);
+
+                try {
+                    return new URL(baseURL, type + "/" + action).toString();
+                } catch (MalformedURLException e) {
+                    throw new RuntimeException("Failed while building newURL", e);
+                }
+            }
+        });
+
+        handlebars.registerHelper("t", new Helper<Object>() {
+            @Override
+            public CharSequence apply(final Object context, final Options options) {
+                String key = Arrays.stream(options.params).map(Object::toString).collect(Collectors.joining("_"));
+                return i18n.t(key);
+            }
+        });
+
+        handlebars.registerHelper("selected", new Helper<Object>() {
+            @Override
+            public CharSequence apply(final Object context, final Options options) {
+                String option = options.param(0);
+                String value = options.param(1);
+
+                return option.equals(value) ? "selected" : "";
+            }
+        });
+
+        return handlebars;
+    }
+}
