f8306e682bb33799913a567d6fd7298c250905f9
Mark Triggs <mark@dishevelled.net>
2017-10-02 14:09:21 +1100

    Batch add requests

--- drive/drive-tool/src/java/org/sakaiproject/drive/tool/GoogleClient.java
+++ drive/drive-tool/src/java/org/sakaiproject/drive/tool/GoogleClient.java
@@ -25,6 +25,7 @@
 package org.sakaiproject.drive.tool;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
@@ -58,12 +59,23 @@ import java.io.InputStreamReader;
 import java.io.File;
 
 import java.net.URL;
+import com.google.api.client.googleapis.services.AbstractGoogleClient;
+import com.google.api.client.googleapis.batch.json.JsonBatchCallback;
+import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest;
+import java.util.LinkedList;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.googleapis.batch.BatchRequest;
+import java.util.Iterator;
 
-/**
- * A handler for the index page in the PA System administration tool.
- */
+// FIXME: split this class up.  Maybe want a .google package?
 public class GoogleClient {
     private static final String APPLICATION = "Sakai Drive";
+
+    private int requestsPerBatch = 100;
+
+    // FIXME: What should this number be?
+    private RateLimiter rateLimiter = new RateLimiter(100000, 100000);
 
     private HttpTransport httpTransport = null;
     private JacksonFactory jsonFactory = null;
@@ -75,8 +87,6 @@ public class GoogleClient {
             jsonFactory = JacksonFactory.getDefaultInstance();
             clientSecrets = GoogleClientSecrets.load(jsonFactory,
                     new InputStreamReader(new FileInputStream(ServerConfigurationService.getSakaiHomePath() + "/client_secrets.json")));
-
-;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -98,6 +108,10 @@ public class GoogleClient {
                 .build();
     }
 
+
+    public void rateLimitHit() {
+        rateLimiter.rateLimitHit();
+    }
 
     public Credential getCredential(String user) {
         try {
@@ -153,4 +167,143 @@ public class GoogleClient {
                 .build();
     }
 
+
+    public LimitedBatchRequest getBatch(AbstractGoogleClient client) throws Exception {
+        return new LimitedBatchRequest(client);
+    }
+
+    public class LimitedBatchRequest {
+
+        // According to the docs, Google sets their maximum to 1000, but we
+        // can't really use that without hitting the rate limit.  See
+        // RateLimiter above.
+        private LinkedList<AbstractGoogleJsonClientRequest> requests = new LinkedList<>();
+        private LinkedList<JsonBatchCallback> callbacks = new LinkedList<>();
+        private AbstractGoogleClient client;
+
+
+        public LimitedBatchRequest(AbstractGoogleClient client) throws Exception {
+            requests = new LinkedList<AbstractGoogleJsonClientRequest>();
+            callbacks = new LinkedList<JsonBatchCallback>();
+
+            this.client = client;
+        }
+
+        public void queue(AbstractGoogleJsonClientRequest request, JsonBatchCallback callback) {
+            requests.add(request);
+            callbacks.add(callback);
+        }
+
+        public void execute() throws Exception {
+            if (requests.isEmpty()) {
+                return;
+            }
+
+            while (executeNextBatch()) {
+                // To glory!
+            }
+        }
+
+        private boolean executeNextBatch() throws Exception {
+            if (requests.isEmpty()) {
+                return false;
+            }
+
+            BatchRequest batch = client.batch(new HttpRequestInitializer() {
+                @Override
+                public void initialize(HttpRequest request) {
+                    request.setConnectTimeout(15 * 60000);
+                    request.setReadTimeout(15 * 60000);
+                }
+            });
+
+            for (int i = 0; !requests.isEmpty() && i < GoogleClient.this.requestsPerBatch; i++) {
+                AbstractGoogleJsonClientRequest request = requests.pop();
+                JsonBatchCallback callback = callbacks.pop();
+
+                request.queue(batch, callback);
+            }
+
+            if (batch.size() > 0) {
+                GoogleClient.this.rateLimiter.wantQueries(batch.size());
+
+                long start = System.currentTimeMillis();
+                // logger.info("Executing batch of size: {}", batch.size());
+                batch.execute();
+                // logger.info("Batch finished in {} ms", System.currentTimeMillis() - start);
+            }
+
+            return !requests.isEmpty();
+        }
+    }
+
+    private class RateLimiter {
+        private long queriesPerTimestep;
+        private long timestepMs;
+
+        public RateLimiter(long queriesPerTimestep, long timestepMs) {
+            this.queriesPerTimestep = queriesPerTimestep;
+            this.timestepMs = timestepMs;
+        }
+
+        // Google limits to 1500 queries per 100 seconds by default.  This
+        // appears to include the subqueries of batch requests (i.e. a single
+        // batch request doesn't just count as one query.)
+        private List<Long> times = new ArrayList<>();
+        private Map<Long, Long> queryCounts = new HashMap<>();
+
+        // Express an interest in running `count` queries.  Block until that's
+        // OK.
+        public synchronized void wantQueries(long count) {
+            if (count > queriesPerTimestep) {
+                throw new RuntimeException("Can't execute that many concurrent queries: " + count);
+            }
+
+            while ((queriesInLastTimestep() + count) >= queriesPerTimestep) {
+                // logger.warn("Waiting for rate limiter to allow another {} queries", count);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {}
+            }
+
+            // OK!
+            recordQueries(count);
+        }
+
+        private void recordQueries(long count) {
+            long now = System.currentTimeMillis();
+
+            if (times.contains(now)) {
+                queryCounts.put(now, queryCounts.get(now) + count);
+            } else {
+                times.add(now);
+                queryCounts.put(now, count);
+            }
+        }
+
+
+        private long queriesInLastTimestep() {
+            long result = 0;
+            long timestepStart = System.currentTimeMillis() - timestepMs;
+
+            Iterator<Long> it = times.iterator();
+            while (it.hasNext()) {
+                long time = it.next();
+
+                if (time < timestepStart) {
+                    // Time expired.  No longer needed.
+                    it.remove();
+                    queryCounts.remove(time);
+                } else {
+                    result += queryCounts.get(time);
+                }
+            }
+
+            return result;
+        }
+
+        public void rateLimitHit() {
+            // logger.warn("Google rate limit hit!");
+        }
+    }
 }
