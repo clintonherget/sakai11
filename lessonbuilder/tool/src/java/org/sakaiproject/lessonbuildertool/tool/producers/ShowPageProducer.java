e358f0b01c2e39c0f1a20933e36b1a919671c946
Payten Giles <payten.giles@gmail.com>
2017-10-30 13:33:22 +1100

    CLASSES-3076 hide the Tool Content and Lessons Content sections on the Add Content dialog when on a student page

--- lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ShowPageProducer.java
+++ lessonbuilder/tool/src/java/org/sakaiproject/lessonbuildertool/tool/producers/ShowPageProducer.java
@@ -3885,6 +3885,10 @@ public class ShowPageProducer implements ViewComponentProducer, DefaultView, Nav
 			createToolBarLink(BltiPickerProducer.VIEW_ID, tofill, "add-blti", "simplepage.blti", currentPage, "simplepage.blti.tooltip");
 		    }
 			}
+
+			// CLASSES-3076 only show these sections when *not* a student page
+			UIOutput.make(tofill, "addContentToolContentSubSection");
+			UIOutput.make(tofill, "addContentLessonsContentSubSection");
 		}
 	}
 
