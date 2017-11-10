b9532811312b2d383e90d11446eda3d956a034b0
bhaveshvasandani <bhavesh@nyu.edu>
2017-11-10 14:24:24 -0500

    Adding Select2 Jquery library

---
+++ stealth/stealth-tool/tool/src/java/org/sakaiproject/stealth/tool/pages/js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	$("#select-term").select2();
+	$("#select-tool").select2();
+    $(".unstealth-multiple-netid").select2({
+        placeholder:"NetID"
+    });
+    $(".unstealth-multiple-siteid").select2({
+        placeholder:"SiteID"
+    });
+});
+function openTab(evt, TabName) {
+	var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+	   tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	   tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(TabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
