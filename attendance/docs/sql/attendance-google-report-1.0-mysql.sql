720e6385b162f2bceaf3262e05652bda97f5c7f3
Payten Giles <payten.giles@gmail.com>
2018-08-14 12:05:06 +1000

    Move stuff back into the attendance job; add some db scripts

---
+++ attendance/docs/sql/attendance-google-report-1.0-mysql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `attendance_record_override_t` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `NETID` varchar(255) NOT NULL,
+  `SITE_ID` varchar(255) NOT NULL,
+  `EVENT_NAME` varchar(255) NOT NULL,
+  `STATUS` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `UNIQ_ATT_REC_O` UNIQUE (`NETID`, `SITE_ID`, `EVENT_NAME`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
