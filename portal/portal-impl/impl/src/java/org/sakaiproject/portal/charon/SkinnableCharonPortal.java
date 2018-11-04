payten/sakai

CLASSES-2964 Don't log a stack trace on every admin request

portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -611,7 +611,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 			ToolConfiguration memberships = userSite.getToolForCommonId("sakai.membership");
 			return String.format("/portal/site/~%s/tool/%s", userid, memberships.getId());
 		} catch (Exception e) {
-			M_log.warn("Couldn't find a memberships tool for user {}", userid, e);
+			M_log.warn("Couldn't find a memberships tool for user {}", userid);
 
 			return "javascript:void(0);";
 		}

clintonherget/sakai11

portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/SkinnableCharonPortal.java
@@ -609,7 +609,7 @@ public class SkinnableCharonPortal extends HttpServlet implements Portal
 			ToolConfiguration memberships = userSite.getToolForCommonId("sakai.membership");
 			return String.format("/portal/site/~%s/tool/%s", userid, memberships.getId());
 		} catch (Exception e) {
-			M_log.warn("Couldn't find a memberships tool for user {}", userid, e);
+			log.warn("Couldn't find a memberships tool for user {}", userid);
 
 			return "javascript:void(0);";
 		}