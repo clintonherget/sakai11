payten/sakai

CLASSES-2906 Pull the description from the template

site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -16518,8 +16518,15 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 		// the section.
 
 		if (isDescriptionFromTemplate(templateSiteId)) {
-			// Do nothing
 			M_log.info("Site description will be taken from template: " + templateSiteId);
+			try {
+				Site templateSite = SiteService.getSite(templateSiteId);
+
+				siteInfo.short_description = templateSite.getShortDescription();
+				siteInfo.description = templateSite.getDescription();
+			} catch (IdUnusedException e) {
+				M_log.error("Site lookup failed" + e);
+			}
 		} else {
 			M_log.info("Site description will be taken from section: " + sectionEid);
 

clintonherget/sakai11

site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -15949,8 +15949,15 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 		// the section.
 
 		if (isDescriptionFromTemplate(templateSiteId)) {
-			// Do nothing
 			log.info("Site description will be taken from template: " + templateSiteId);
+			try {
+				Site templateSite = SiteService.getSite(templateSiteId);
+
+				siteInfo.short_description = templateSite.getShortDescription();
+				siteInfo.description = templateSite.getDescription();
+			} catch (IdUnusedException e) {
+				log.error("Site lookup failed" + e);
+			}
 		} else {
 			log.info("Site description will be taken from section: " + sectionEid);
 