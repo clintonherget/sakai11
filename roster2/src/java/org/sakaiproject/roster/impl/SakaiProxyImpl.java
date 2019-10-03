885f1d75b25f8b5284e47747b5810c14320c8473
Mark Triggs <mark@dishevelled.net>
2019-10-03 15:36:12 +1000

    CLASSES-3773 Revert "CLASSES-3669 - Provide text / link to direct user to Profile for name pronunciation recording, if not recorded"
    This reverts commit 6534ff9c45747397ed6682a22e70f02fa10b7a58.

--- roster2/src/java/org/sakaiproject/roster/impl/SakaiProxyImpl.java
+++ roster2/src/java/org/sakaiproject/roster/impl/SakaiProxyImpl.java
@@ -1272,19 +1272,6 @@ public class SakaiProxyImpl implements SakaiProxy, Observer {
     /**
      * {@inheritDoc}
      */
-    public String getProfileToolLink() {
-        try {
-            Site site = siteService.getSite(siteService.getUserSiteId(getCurrentUserId()));
-            return site.getUrl() + "/tool/" + site.getToolForCommonId("sakai.profile2").getId();
-        } catch(Exception e){
-            log.error("Error getting tool for profile on user workspace {}", e.getMessage());
-        }
-        return null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     public boolean isNamePronunciationEnabledInSite() {
         return isSitePropertyEnabled(NAME_PRONUNCIATION_SITE_PROPERTY);
     }
