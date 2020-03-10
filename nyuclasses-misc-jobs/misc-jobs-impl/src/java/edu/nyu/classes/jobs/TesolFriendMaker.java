b74c6657cdf48c8413e9b5a70759facc2f7c5abf
Mark Triggs <mark@dishevelled.net>
2020-03-10 12:23:52 +1100

    Close prepared statements
    Noticed in logs:
    
    Caused by: java.sql.SQLException: ORA-01000: maximum open cursors exceeded
    
            at oracle.jdbc.driver.T4CTTIoer.processError(T4CTTIoer.java:445)
            at oracle.jdbc.driver.T4CTTIoer.processError(T4CTTIoer.java:396)
            at oracle.jdbc.driver.T4C8Oall.processError(T4C8Oall.java:879)
            at oracle.jdbc.driver.T4CTTIfun.receive(T4CTTIfun.java:450)
            at oracle.jdbc.driver.T4CTTIfun.doRPC(T4CTTIfun.java:192)
            at oracle.jdbc.driver.T4C8Oall.doOALL(T4C8Oall.java:531)
            at oracle.jdbc.driver.T4CPreparedStatement.doOall8(T4CPreparedStatement.java:207)
            at oracle.jdbc.driver.T4CPreparedStatement.executeForRows(T4CPreparedStatement.java:1044)
            at oracle.jdbc.driver.OracleStatement.doExecuteWithTimeout(OracleStatement.java:1329)
            at oracle.jdbc.driver.OraclePreparedStatement.executeInternal(OraclePreparedStatement.java:3584)
            at oracle.jdbc.driver.OraclePreparedStatement.executeUpdate(OraclePreparedStatement.java:3665)
            at oracle.jdbc.driver.OraclePreparedStatementWrapper.executeUpdate(OraclePreparedStatementWrapper.java:1352)
            at com.zaxxer.hikari.pool.ProxyPreparedStatement.executeUpdate(ProxyPreparedStatement.java:61)
            at com.zaxxer.hikari.pool.HikariProxyPreparedStatement.executeUpdate(HikariProxyPreparedStatement.java)
            at edu.nyu.classes.jobs.TesolFriendMaker$TesolFriendMakerJob.handleDeletes(TesolFriendMaker.java:203)
            at edu.nyu.classes.jobs.TesolFriendMaker$TesolFriendMakerJob.execute(TesolFriendMaker.java:129)

--- nyuclasses-misc-jobs/misc-jobs-impl/src/java/edu/nyu/classes/jobs/TesolFriendMaker.java
+++ nyuclasses-misc-jobs/misc-jobs-impl/src/java/edu/nyu/classes/jobs/TesolFriendMaker.java
@@ -198,9 +198,13 @@ public class TesolFriendMaker {
                 delete_friend.setString(4, p.user1);
                 delete_friend.executeUpdate();
 
+                delete_friend.close();
+
                 PreparedStatement delete_tesol = connection.prepareStatement("delete from TESOL_CONNECTION where connection_key = ?");
                 delete_tesol.setString(1, p.getKey());
                 delete_tesol.executeUpdate();
+
+                delete_tesol.close();
             }
         }
 
@@ -280,6 +284,8 @@ public class TesolFriendMaker {
                         result.add(p);
                     }
                 }
+
+                ps.close();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -311,6 +317,8 @@ public class TesolFriendMaker {
                 while (rs.next()) {
                     excludedKeys.add(rs.getString("connection_key"));
                 }
+
+                ps.close();
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
