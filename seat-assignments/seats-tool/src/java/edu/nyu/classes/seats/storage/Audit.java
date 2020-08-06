30605d9bb74541b6359c5aa251884439cdb76b69
Mark Triggs <mark@dishevelled.net>
2020-08-06 19:14:24 +1000

    Split audit handling into its own file

---
+++ seat-assignments/seats-tool/src/java/edu/nyu/classes/seats/storage/Audit.java
@@ -0,0 +1,27 @@
+package edu.nyu.classes.seats.storage;
+
+import java.sql.SQLException;
+
+public class Audit {
+    public enum AuditEvents {
+        SEAT_CLEARED,
+        SEAT_ASSIGNED,
+        SEAT_REASSIGNED,
+        MEETING_DELETED,
+        GROUP_DELETED,
+        GROUP_CREATED,
+        MEETING_CREATED,
+        MEMBER_ADDED,
+    }
+
+    public static void insert(DBConnection db, AuditEvents event, String json, String args[]) throws SQLException {
+        long timestamp = System.currentTimeMillis();
+
+        db.run("insert into seat_audit (id, timestamp_ms, event_code, json) values (?, ?, ?, ?)")
+            .param(db.uuid())
+            .param(timestamp)
+            .param(event.toString())
+            .param(json)
+            .executeUpdate();
+    }
+}
