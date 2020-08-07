b943c6c4b10ab17280a9fd7d22b4f44af14f99bc
Mark Triggs <mark@dishevelled.net>
2020-08-07 13:07:30 +1000

    Record member roles and don't add instructors to groups

--- seat-assignments/seats-tool/src/java/edu/nyu/classes/seats/models/Member.java
+++ seat-assignments/seats-tool/src/java/edu/nyu/classes/seats/models/Member.java
@@ -1,5 +1,7 @@
 package edu.nyu.classes.seats.models;
 
+import java.util.Locale;
+
 public class Member {
     public enum Modality {
         ONLINE,
@@ -10,11 +12,35 @@ public class Member {
     public String netid;
     public Modality modality;
     public boolean official;
+    public Role role;
 
-    public Member(String netid, boolean official) {
+    public enum Role {
+        INSTRUCTOR,
+        COURSE_SITE_ADMIN,
+        STUDENT,
+        TEACHING_ASSISTANT;
+
+        public static Role forCMRole(String cmRole) {
+            cmRole = cmRole.toUpperCase(Locale.ROOT);
+
+            if ("I".equals(cmRole)) {
+                return INSTRUCTOR;
+            } else if ("S".equals(cmRole)) {
+                return STUDENT;
+            } else if ("A".equals(cmRole)) {
+                return COURSE_SITE_ADMIN;
+            } else {
+                // Shouldn't happen, but student is a safe default.
+                return STUDENT;
+            }
+        }
+    }
+
+    public Member(String netid, boolean official, Role role) {
         this.netid = netid;
         this.modality = Modality.values()[(int)Math.floor(Math.random() * 3)];
         this.official = official;
+        this.role = role;
     }
 
     public int hashCode() {
@@ -27,5 +53,9 @@ public class Member {
         }
 
         return ((Member) other).netid.equals(netid);
+    }
+
+    public boolean isInstructor() {
+        return Role.INSTRUCTOR.equals(role) || Role.COURSE_SITE_ADMIN.equals(role);
     }
 }
