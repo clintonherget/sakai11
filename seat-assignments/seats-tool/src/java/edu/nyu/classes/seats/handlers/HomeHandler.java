9879b5b05205250b044294a9187b1beb541b9971
Payten Giles <payten.giles@gmail.com>
2020-08-07 10:42:34 +1000

    Build a reasonable section name

--- seat-assignments/seats-tool/src/java/edu/nyu/classes/seats/handlers/HomeHandler.java
+++ seat-assignments/seats-tool/src/java/edu/nyu/classes/seats/handlers/HomeHandler.java
@@ -1,46 +1,10 @@
 package edu.nyu.classes.seats.handlers;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.TimeZone;
-import java.util.Comparator;
-import java.text.DateFormat;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.sakaiproject.component.cover.ServerConfigurationService;
-import org.sakaiproject.tool.cover.SessionManager;
-import org.sakaiproject.util.ResourceLoader;
-import org.sakaiproject.time.cover.TimeService;
-import org.sakaiproject.time.api.Time;
-
-import java.util.stream.Collectors;
-import java.util.Collections;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-
-import java.net.URL;
-
-import org.sakaiproject.component.cover.ComponentManager;
-import org.sakaiproject.content.api.ContentEntity;
-import org.sakaiproject.content.api.ContentHostingService;
-import org.sakaiproject.content.api.ContentCollection;
-import org.sakaiproject.content.api.ContentResource;
-import org.sakaiproject.content.api.ContentTypeImageService;
-import org.sakaiproject.entity.api.ResourceProperties;
-import org.sakaiproject.entity.api.EntityPropertyNotDefinedException;
-import org.sakaiproject.entity.api.EntityPropertyTypeException;
-import org.sakaiproject.user.cover.UserDirectoryService;
-import org.sakaiproject.user.api.UserNotDefinedException;
-import org.sakaiproject.content.api.GroupAwareEntity.AccessMode;
-
-
-import org.sakaiproject.site.api.Group;
-import org.sakaiproject.site.api.Site;
-import org.sakaiproject.site.cover.SiteService;
-import org.sakaiproject.authz.api.Member;
 
 public class HomeHandler implements Handler {
 
